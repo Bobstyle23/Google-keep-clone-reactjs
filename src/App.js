313f2256d49578b3d01229309f591581855ccdc9
Bob Pakhriev <boburpakhriev@gmail.com>
2021-06-13 16:04:47 +0900

    Change function into arrow function

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import Footer from "./components/Footer";
 import Note from "./components/Note";
 import notes from "./notes";
 
-function createNote(notes) {
-  return <Note key={notes.key} title={notes.title} content={notes.content} />;
-}
 function App() {
   return (
     <div className="App">
       <Header />
       <Footer />
-      {notes.map(createNote)}
+      {notes.map((notes) => (
+        <Note key={notes.key} title={notes.title} content={notes.content} />
+      ))}
+      ;
     </div>
   );
 }
